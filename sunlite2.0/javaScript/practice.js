6ddc2c12a643d388549d86f0724af720498060a0
moshe <105514675+woodenperl@users.noreply.github.com>
2022-06-10 10:51:54 -0400

    Delete sunlite2.0 directory

--- sunlite2.0/javaScript/practice.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('press').onclick = function changeContent() {
-
-    document.getElementById('addded').innerHTML = "Hello World";
-    document.getElementById('demo').style = "Color: black";
- 
- }
