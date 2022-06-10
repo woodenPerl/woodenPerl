950001a3564e91db8154a4103b864d638ff5777c
moshe <105514675+woodenperl@users.noreply.github.com>
2022-06-10 10:54:24 -0400

    Add files via upload
    initial commit

---
+++ sunlite2.0/javaScript/practice.js
@@ -0,0 +1,6 @@
+document.getElementById('press').onclick = function changeContent() {
+
+    document.getElementById('addded').innerHTML = "Hello World";
+    document.getElementById('demo').style = "Color: black";
+ 
+ }
